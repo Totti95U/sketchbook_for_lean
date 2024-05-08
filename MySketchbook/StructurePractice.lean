5c6b857c6263b5f812dc6d481d929c39d70e1c55
ToshikiUSUI <toshiki,940@s.kyushu-u.ac.jp>
2024-05-08 16:15:25 +0900

    done section 6.2

--- MySketchbook/StructurePractice.lean
+++ MySketchbook/StructurePractice.lean
@@ -79,27 +79,31 @@ namespace Point
     rw [add, add, smul, smul, smul]
     ext <;> dsimp
     repeat' rw [mul_add]
+end Point
 
-  -- `structure` にはデータが満たすべき "制約" をつけることができる
-  -- `StandardTwoSimplex` は標準2単体の点を表す構造である
-  structure StandardTwoSimplex where
-    x : ℝ
-    y : ℝ
-    z : ℝ
-    x_nonneg : 0 ≤ x
-    y_nonneg : 0 ≤ y
-    z_nonneg : 0 ≤ z
-    sum_eq : x + y + z = 1
+#check Point.add myPoint1 myPoint2
+#check myPoint1.add myPoint2
 
-  -- そのような構造に関する関数は例えば次のように定義される
-  def swapXy (a : StandardTwoSimplex) : StandardTwoSimplex where
-    x := a.y
-    y := a.x
-    z := a.z
-    x_nonneg := a.y_nonneg
-    y_nonneg := a.x_nonneg
-    z_nonneg := a.z_nonneg
-    sum_eq := by rw [add_comm a.y a.x, a.sum_eq]
+-- `structure` にはデータが満たすべき "制約" をつけることができる
+-- `StandardTwoSimplex` は標準2単体の点を表す構造である
+structure StandardTwoSimplex where
+  x : ℝ
+  y : ℝ
+  z : ℝ
+  x_nonneg : 0 ≤ x
+  y_nonneg : 0 ≤ y
+  z_nonneg : 0 ≤ z
+  sum_eq : x + y + z = 1
+
+-- そのような構造に関する関数は例えば次のように定義される
+def swapXy (a : StandardTwoSimplex) : StandardTwoSimplex where
+  x := a.y
+  y := a.x
+  z := a.z
+  x_nonneg := a.y_nonneg
+  y_nonneg := a.x_nonneg
+  z_nonneg := a.z_nonneg
+  sum_eq := by rw [add_comm a.y a.x, a.sum_eq]
 
 -- 単体内の2点の中点はまた単体に属する
 -- noncomputable は 実数の除算をするためにつけている
@@ -150,6 +154,18 @@ def weightedAverage (lambda : Real) (lambda_nonneg : 0 ≤ lambda) (lambda_le : 
         simp [div_eq_mul_inv, ← Finset.sum_mul, Finset.sum_add_distrib, a.sum_eq_one, b.sum_eq_one]
         field_simp
 
+    -- Finset.sum_mul と Finset.sum_add_distrib を使うといい感じらしい
+    def weightedAverage (n : ℕ) (lambda : ℝ) (lambda_nonneg : 0 ≤ lambda) (lambda_le : lambda ≤ 1) (a b : StandardSimplex n) : StandardSimplex n where
+      V i := lambda * a.V i + (1 - lambda) * b.V i
+      NonNeg := by
+        intro i
+        apply add_nonneg
+        · exact mul_nonneg lambda_nonneg (a.NonNeg i)
+        · exact mul_nonneg (sub_nonneg_of_le lambda_le) (b.NonNeg i)
+
+      sum_eq_one := by
+        sorry
+
   end StandardSimplex
 
   -- 構造を使うことで性質をまとめることもできる
@@ -185,10 +201,190 @@ def weightedAverage (lambda : Real) (lambda_nonneg : 0 ≤ lambda) (lambda_le : 
 
   end
 
+  -- シグマ型の例として標準単体全体の方を紹介しよう
+  def StdSimplex := Σ n : ℕ, StandardSimplex n
+
+  section
+  variable (s : StdSimplex)
+
+  #check s.fst
+  #check s.snd
+
+  #check s.1
+  #check s.2
+
+  end
+  /-
+  上の例でみたように structure の代わりに product 型 や sigma 型を使うこともできる. しかし prod や sigma は各メンバの名前が固定のものになり, またどのような型を持っているのかの認識が難しくなる. structure を使うことで, 各メンバにカスタムネームをつけることができ, さらに型の表記が簡単になる
+  -/
+
+-- 代数的構造の一例として型α上に定まる群耕造を定義してみよう
+structure Group₁ (α : Type*) where
+  mul : α → α → α
+  one : α
+  inv : α → α
+  mul_assoc : ∀ x y z : α, mul (mul x y) z = mul x (mul y z)
+  mul_one : ∀ x : α, mul x one = x
+  one_mul : ∀ x : α, mul one x = x
+  mul_left_inv : ∀ x : α, mul (inv x) x = one
+
+-- ↓これは Mathlib のGroup. ctrl+clickで定義が見れる
+#check Group
+
+-- 便利なので群全体の型も定義しよう
+structure Group₁Cat where
+  α : Type*
+  str : Group₁ α
+
+-- lean において同型は次のように扱う
+section
+variable (α β γ : Type*)
+variable (f : α ≃ β) (g : β ≃ γ)
+
+#check Equiv α β
+#check (f.toFun : α → β)
+#check (f.invFun : β → α)
+#check (f.right_inv : ∀ x : β, f (f.invFun x) = x)
+#check (f.left_inv : ∀ x : α, f.invFun (f x) = x)
+#check (Equiv.refl α : α ≃ α)
+#check (f.symm : β ≃ α)
+#check (f.trans g : α ≃ γ)
+
+-- Mathlibは偉いので同型の合成ができる
+example (x : α) : (f.trans g).toFun x = g.toFun (f.toFun x) := rfl
+
+example (x : α) : (f.trans g) x = g (f x) := rfl
+
+example : (f.trans g : α → γ) = g ∘ f := rfl
+
+example (α : Type*) : Equiv.Perm α = (α ≃ α) := rfl
+
+end
+
+def permGroup {α : Type*} : Group₁ (Equiv.Perm α) where
+  mul f g := Equiv.trans g f
+  one := Equiv.refl α
+  inv := Equiv.symm
+  mul_assoc f g h := (Equiv.trans_assoc _ _ _).symm
+  one_mul := Equiv.refl_trans
+  mul_one := Equiv.refl_trans
+  mul_left_inv := Equiv.self_trans_symm
+
+structure AddGroup₁ (α : Type*) where
+  add : α → α → α
+  zero : α
+  neg : α → α
+  add_assoc : ∀ x y z : α, add (add x y) z = add x (add y  z)
+  zero_add : ∀ x : α, add zero x = x
+  add_zero : ∀ x : α, add x zero = x
+  add_left_neg : ∀ x : α, add (neg x) x = zero
+
+namespace Point
+  def neg (a : Point) : Point :=
+    ⟨-a.x, -a.y, -a.z⟩
+
+  def zero : Point := ⟨0, 0, 0⟩
+
+  def addGroupPoint : AddGroup₁ Point := sorry
+
 end Point
 
-#check Point.add myPoint1 myPoint2
-#check myPoint1.add myPoint2
+/-
+structure を使った型の定義では通常の数学をするのに困難が生じることがある
+たとえば ℝ は Group でも Commutative Ring でも Metric Space でもある
+このように複数の属性を持っていることを Lean に伝えるのに structure を使った定義は難しい
+そこで Lean では `class` `instance` `[]記法` によってそれを実現できるようにサポートしている
+-/
 
+-- `structure` の代わりに `class` を使う
+class Group₂ (α : Type*) where
+  mul : α → α → α
+  one : α
+  inv : α → α
+  mul_assoc : ∀ x y z : α, mul (mul x y) z = mul x (mul y z)
+  mul_one : ∀ x : α, mul x one = x
+  one_mul : ∀ x : α, mul one x = x
+  mul_left_inv : ∀ x : α, mul (inv x) x = one
+
+-- `def` の代わりに `instance` を使う
+instance {α : Type*} : Group₂ (Equiv.Perm α) where
+  mul f g := Equiv.trans g f
+  one := Equiv.refl α
+  inv := Equiv.symm
+  mul_assoc f g h := (Equiv.trans_assoc _ _ _).symm
+  one_mul := Equiv.trans_refl
+  mul_one := Equiv.refl_trans
+  mul_left_inv := Equiv.self_trans_symm
+
+-- `[self : Group₂ α]` が現れる
+#check Group₂.mul
+
+-- `[Group₂ α]` を使うことで `α` には `Group₂` の構造が乗っかっていて, そのことを活用してほしいと Lean に伝えることができる
+def mySquare {α : Type*} [Group₂ α] (x : α) := Group₂.mul x x
+
+#check mySquare
+
+section
+variable {β : Type*} (f g : Equiv.Perm β)
+
+example : Group₂.mul f g = g.trans f := rfl
+
+example : mySquare f = f.trans f := rfl
+
+end
+
+-- `class` と `instance` を使うことで記法を統一することができる
+/-
+例えば, 任意の `Group` インスタンスの演算としては `+`,
+任意の `AddGroup` インスタンスの演算として `*`,
+任意の `Ring` インスタンスの演算としては `+` と `*`
+
+が使えるようになっている. これは `class` と `instance` を用いたことによる恩恵である
+-/
+
+-- 次のようにすることで `Group₂` に記法を追加できる
+
+instance hasMulGroup₂ {α : Type*} [Group₂ α] : Mul α := ⟨Group₂.mul⟩
+
+instance hasOneGroup₂ {α : Type*} [Group₂ α] : One α := ⟨Group₂.one⟩
+
+instance hasInvGroup₂ {α : Type*} [Group₂ α] : Inv α := ⟨Group₂.inv⟩
+
+section
+variable {α : Type*} (f g : Equiv.Perm α)
+
+#check f * 1 * g⁻¹
+
+def foo : f * 1 * g⁻¹ = g.symm.trans ((Equiv.refl α).trans f) := rfl
+
+end
+
+-- 演習問題として AddGroup₂ を実装してみる
+
+class AddGroup₂ (α : Type*) where
+  add : α → α → α
+  zero : α
+  neg : α → α
+  add_assoc : ∀ x y z : α , add (add x y) z = add x (add y z)
+  zero_add : ∀ x : α , add zero x = x
+  add_zero : ∀ x : α , add x zero = x
+  add_left_neg : ∀ x : α , add (neg x) x = zero
+
+instance hasAddAddGroup₂ {α : Type*} [AddGroup₂ α] : Add α := ⟨AddGroup₂.add⟩
+
+instance hasZeroAddGroup₂ {α : Type*} [AddGroup₂ α] : Zero α := ⟨AddGroup₂.zero⟩
+
+instance hasNegAddGroup₂ {α : Type*} [AddGroup₂ α] : Neg α := ⟨AddGroup₂.neg⟩
+
+instance : AddGroup₂ Point where
+  add := Point.add
+  zero := ⟨0, 0, 0⟩
+  neg a := ⟨-a.x, -a.y, -a.z⟩
+  add_assoc := Point.add_assoc
+  zero_add := sorry
+  add_zero := sorry
+  add_left_neg := sorry
+
+-- § 6.3 Building the Gaussian Integers
 
 end my_sketch
